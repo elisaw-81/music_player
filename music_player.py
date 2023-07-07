3c19c9b9b849db153ec78fcc882fdd720dcde1b9
elisaw-81 <e.saghari81@gmail.com>
2023-07-08 00:24:23 +0430

    no message

--- music_player.py
+++ music_player.py
@@ -99,3 +99,44 @@ while running:
             running = False
 
 pygame.quit()
+
+import pygame
+import os
+import keyboard
+
+pygame.init()
+
+music_directory = input("Enter the path to your music directory: ")
+
+music_files = [file for file in os.listdir(music_directory) if file.endswith(".mp3")]
+
+current_track = 0
+pygame.mixer.init()
+
+pygame.mixer.music.load(os.path.join(music_directory, music_files[current_track]))
+
+pygame.mixer.music.play()
+
+running = True
+while running:
+    if keyboard.is_pressed("space"):
+        if pygame.mixer.music.get_busy():
+            pygame.mixer.music.pause()
+        else:
+            pygame.mixer.music.unpause()
+
+    if keyboard.is_pressed("right"):
+        current_track = (current_track + 1) % len(music_files)
+        pygame.mixer.music.load(os.path.join(music_directory, music_files[current_track]))
+        pygame.mixer.music.play()
+
+    if keyboard.is_pressed("left"):
+        current_track = (current_track - 1) % len(music_files)
+        pygame.mixer.music.load(os.path.join(music_directory, music_files[current_track]))
+        pygame.mixer.music.play()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
